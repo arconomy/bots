cfe4ec61b5b3e97944e77cc65f68658680b6ee32
Alistair Cornish <Alistair Cornish>
2017-08-09 17:12:32 +1000

    Added Time Syncher Bot

---
+++ Robots/Time Syncher/Time Syncher/Time Syncher.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using cAlgo.API;
+using cAlgo.API.Indicators;
+using cAlgo.API.Internals;
+using cAlgo.Indicators;
+
+namespace cAlgo
+{
+    [Robot(TimeZone = TimeZones.UTC, AccessRights = AccessRights.None)]
+    public class TimeSyncher : Robot
+    {
+        protected override void OnStart()
+        {
+
+        }
+
+        protected override void OnTick()
+        {
+            Print("System Clock : " + System.DateTime.UtcNow);
+            Print("Server Clock : " + Server.Time);
+            Print("---------------------------------");
+        }
+
+        protected override void OnStop()
+        {
+            // Put your deinitialization logic here
+        }
+    }
+}
